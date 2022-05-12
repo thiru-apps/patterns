f89b5a4d809cfd85bc5a8007239c205fb1ba331a
Thiru <dmthiruna@gmail.com>
2022-05-12 08:04:32 +0530

    initial version

---
+++ src/test/java/com/design/patterns/PatternsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.design.patterns;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatternsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
